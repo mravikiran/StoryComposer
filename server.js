5c97dd1c5cd6b28b11043908fbb7a20e2bd38080
ravi kiran mikkilineni <ravikiran1906@gmail.com>
2014-07-12 03:59:22 -0500

    adding server in root

---
+++ server.js
@@ -0,0 +1,19 @@
+// test server
+
+var http = require("http");
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+var server =  http.createServer(
+function(request, response){
+	response.writeHead(200, {"Content-Type": "text/plain"});
+	response.write("Will we ever get rid of hello world");
+	response.end();
+}
+);
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", server_port " + server_port )
+});
+
+
