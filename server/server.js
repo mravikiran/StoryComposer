1522eb2fb5e18f6c123b7afcb4ab2a7895664706
ravi kiran mikkilineni <ravikiran1906@gmail.com>
2014-07-12 03:53:13 -0500

    adding open shift info

--- server/server.js
+++ server/server.js
@@ -2,10 +2,18 @@
 
 var http = require("http");
 
-http.createServer(
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+var server =  http.createServer(
 function(request, response){
 	response.writeHead(200, {"Content-Type": "text/plain"});
 	response.write("Will we ever get rid of hello world");
 	response.end();
 }
-).listen(8888);
+);
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", server_port " + server_port )
+});
+
+
